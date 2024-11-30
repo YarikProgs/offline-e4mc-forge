dd93444c6ab78fce9896e24c30b9da240af20530
YarikProgs <z.yaroslav2009@gmail.com>
2024-11-30 18:57:50 +0300

    feat: ported to forge 1.20.1, added .gitignore

--- src/main/java/com/syanicxd/e4mcoffline/E4MCOOfflineMod.java
+++ src/main/java/com/syanicxd/e4mcoffline/E4MCOOfflineMod.java
@@ -1,14 +1,18 @@
 package com.syanicxd.e4mcoffline;
 
-import net.fabricmc.api.ModInitializer;
+import net.minecraftforge.fml.common.Mod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class E4MCOOfflineMod implements ModInitializer {
-    public static final Logger LOGGER = LoggerFactory.getLogger("e4mc-offline");
+import static com.syanicxd.e4mcoffline.E4MCOOfflineMod.MOD_ID;
 
-    @Override
-    public void onInitialize() {
+@SuppressWarnings("unused")
+@Mod(MOD_ID)
+public class E4MCOOfflineMod {
+    public static final String MOD_ID = "e4mcoffline";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    public E4MCOOfflineMod() {
         LOGGER.info("E4MC Offline Mod initialized");
     }
 }
--- src/main/java/com/syanicxd/e4mcoffline/mixin/IntegratedServerMixin.java
+++ src/main/java/com/syanicxd/e4mcoffline/mixin/IntegratedServerMixin.java
@@ -1,10 +1,9 @@
 package com.syanicxd.e4mcoffline.mixin;
 
-import net.minecraft.server.integrated.IntegratedServer;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.integrated.IntegratedServer;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,10 +15,11 @@ public class IntegratedServerMixin {
     @Inject(method = "openToLan", at = @At("HEAD"))
     private void onOpenToLan(CallbackInfoReturnable<Boolean> cir) {
         // Set online-mode to false when opening to LAN
-        ((IntegratedServer)(Object)this).setOnlineMode(false);
+        ((IntegratedServer) (Object) this).setOnlineMode(false);
 
         // Notify stuff
-        MinecraftServer server = (MinecraftServer)(Object)this;
+        //noinspection DataFlowIssue
+        MinecraftServer server = (MinecraftServer) (Object) this;
         String message = "[§coffline-e4mc§r] Server allowed offline/cracked players to join §aSuccessfully§r";
 
         for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
